7d9335bc2f69316c78507ebdb167737064155f3a
Fridfinnur <fmt1@hi.is>
2022-07-25 11:58:51 +0000

    Add PWM turn-on and h-bridge braking

--- src/board.cpp
+++ src/board.cpp
@@ -21,26 +21,49 @@ void setup_gpio() {
   digitalWrite(PIN_VALVE_2_REV, LOW);
 }
 
-void power_on() { digitalWrite(PIN_PWR_EN, HIGH); }
+void power_on() {
+  const int channel = 1;
+  ledcSetup(channel, 1E6, 6);
+  ledcAttachPin(PIN_PWR_EN, channel);
+  for (int duty = 0; duty < 64; duty++) {
+    ledcWrite(channel, duty);
+    delayMicroseconds(1000);
+  }
+  ledcDetachPin(PIN_PWR_EN);
+  digitalWrite(PIN_PWR_EN, HIGH);
+}
+
 void power_off() { digitalWrite(PIN_PWR_EN, LOW); }
 
 void open_valves() {
   digitalWrite(PIN_VALVE_1_REV, HIGH);
   vTaskDelay(200 / portTICK_PERIOD_MS);
-  digitalWrite(PIN_VALVE_1_REV, LOW);
+  digitalWrite(PIN_VALVE_1_FWD, HIGH);
   vTaskDelay(200 / portTICK_PERIOD_MS);
+  digitalWrite(PIN_VALVE_1_FWD, LOW);
+  digitalWrite(PIN_VALVE_1_REV, LOW);
+  delay(1000);
   digitalWrite(PIN_VALVE_2_REV, HIGH);
   vTaskDelay(200 / portTICK_PERIOD_MS);
+  digitalWrite(PIN_VALVE_2_FWD, HIGH);
+  vTaskDelay(200 / portTICK_PERIOD_MS);
+  digitalWrite(PIN_VALVE_2_FWD, LOW);
   digitalWrite(PIN_VALVE_2_REV, LOW);
 }
 
 void close_valves() {
   digitalWrite(PIN_VALVE_1_FWD, HIGH);
   vTaskDelay(200 / portTICK_PERIOD_MS);
-  digitalWrite(PIN_VALVE_1_FWD, LOW);
+  digitalWrite(PIN_VALVE_1_REV, HIGH);
   vTaskDelay(200 / portTICK_PERIOD_MS);
+  digitalWrite(PIN_VALVE_1_REV, LOW);
+  digitalWrite(PIN_VALVE_1_FWD, LOW);
+  delay(1000);
   digitalWrite(PIN_VALVE_2_FWD, HIGH);
   vTaskDelay(200 / portTICK_PERIOD_MS);
+  digitalWrite(PIN_VALVE_2_REV, HIGH);
+  vTaskDelay(200 / portTICK_PERIOD_MS);
+  digitalWrite(PIN_VALVE_2_REV, LOW);
   digitalWrite(PIN_VALVE_2_FWD, LOW);
 }
 
